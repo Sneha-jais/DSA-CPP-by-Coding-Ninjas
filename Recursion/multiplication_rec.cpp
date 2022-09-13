917208d89b0a4e897c89cbeb2fb494f7c9f840f8
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2022-09-13 21:40:15 +0530

    Update multiplication_rec.cpp

--- Recursion/multiplication_rec.cpp
+++ Recursion/multiplication_rec.cpp
@@ -1,3 +1,22 @@
+/* :)
+Given two integers M & N, calculate and return their multiplication using recursion. You can only use subtraction and addition for your calculation. No other operators are allowed.
+Input format :
+Line 1 : Integer M
+Line 2 : Integer N
+
+Output format :
+M x N
+
+Sample Input 1 :
+3 
+5
+
+Sample Output 1 :
+15
+*/
+
+
+
 #include<iostream>
 using namespace std;
 int mul(int x,int y,int ans){
