c40a7732f00b3f1fb5a6dc4379166fe4978093db
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2022-09-13 21:39:17 +0530

    Update geometric_sum.cpp

--- Recursion/geometric_sum.cpp
+++ Recursion/geometric_sum.cpp
@@ -1,3 +1,22 @@
+/*  :)
+Given k, find the geometric sum i.e.
+1 + 1/2 + 1/4 + 1/8 + ... + 1/(2^k)
+using recursion.
+Input format : Integer k
+
+Output format : Geometric sum
+Constraints :
+0 <= k <= 1000
+
+Sample Input 1 :
+3
+
+Sample Output 1 :
+1.875
+*/
+
+
+
 #include<iostream>
 #include<math.h>
 using namespace std;
