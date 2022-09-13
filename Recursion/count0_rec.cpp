18965ee2b9c8d2732cc33eb34292604daae2e020
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2022-09-13 21:41:01 +0530

    Update count0_rec.cpp

--- Recursion/count0_rec.cpp
+++ Recursion/count0_rec.cpp
@@ -1,3 +1,22 @@
+/* :)
+Given an integer N, count and return the number of zeros that are present in the given integer using recursion.
+Input Format :
+Integer N
+
+Output Format :
+Number of zeros in N
+
+Constraints :
+0 <= N <= 10^9
+
+Sample Input 1 :
+10204
+
+Sample Output 1 :
+2
+*/
+
+
 #include <iostream>
 using namespace std;
 
