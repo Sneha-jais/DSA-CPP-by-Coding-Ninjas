f4e64b1acec84c343ec5a2cb4ea6a85d429c77c6
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2022-09-30 00:17:37 +0530

    Add files via upload

---
+++ lecture8_LinkedList1/recursive_deletion.cpp
@@ -0,0 +1,83 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+Node *takeInput()
+{
+
+    int data;
+    cin >> data;
+
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    while (data != -1)
+    {
+
+        Node *newNode = new Node(data);
+
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = tail->next;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
+void rec_delet(Node* &head, int val)
+{
+    if (head == NULL)
+    {
+        cout << "Element not present in the list\n";
+        return;
+    }
+    if (head->data == val)
+    {
+        Node *temp = head;
+        head = head->next;
+        delete (temp);
+        return;
+    }
+    rec_delet(head->next, val);
+}
+void print(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+
+    Node *head = takeInput();
+    int value;
+    cin >> value;
+    rec_delet(head, value);
+    print(head);
+
+    return 0;
+}
