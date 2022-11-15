669ea0a7d45a7a926c162ea550a103766a1f9014
Sneha Jaiswal <78521605+Sneha-jais@users.noreply.github.com>
2022-11-16 00:39:38 +0530

    Update deletion_of_a_node_from_LL.cpp

--- lecture8_LinkedList1/deletion_of_a_node_from_LL.cpp
+++ lecture8_LinkedList1/deletion_of_a_node_from_LL.cpp
@@ -1,8 +1,6 @@
 /* :)
 You have been given a linked list of integers. Your task is to write a function that deletes a node from a given position, 'pos'.
-Note :
-Assume that the Indexing for the linked list always starts from 0.
-
+Note :Assume that the Indexing for the linked list always starts from 0.
 If the position is greater than or equal to the length of the linked list, you should return the same linked list without any change.
 Sample Input 1 :
 1 
